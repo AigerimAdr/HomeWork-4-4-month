9b99a22a70a2ea0758d7d002958be6dbaee6dcb3
AigerimAdr <aigerim210596@gmail.com>
2023-07-25 22:17:47 +0600

    Выполнила ДЗ-4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./FormRegister.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class FormRegister extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: "",
+            number: "",
+            email: "",
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.name, this.state.number, this.state.email)
+    };
+
+    render() {
+        return(
+            <div className="form-container">
+            <form onSubmit={this.handleSubmit} className="form-sumbit">
+                <input
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                    placeholder="Full Name"
+                    className="input-field"
+                />
+                <input
+                    type="text"
+                    name="number"
+                    value={this.state.number}
+                    onChange={this.handleChange}
+                    placeholder="Phone number"
+                    className="input-field"
+                />
+                <input
+                    type="email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    placeholder="Email"
+                    className="input-field"
+                />
+                <button type="submit" className="submit-button">Submit</button>
+            </form>
+        </div>
+        )
+    }
 }
-
-export default App;
